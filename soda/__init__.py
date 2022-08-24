77ffc326c1e5e81e6b2ecc452b9eef19e10f614a
Jelle van Assema <jelle.van.assema@gmail.com>
2022-08-24 12:32:02 +0000

    soda checks

--- soda/__init__.py
+++ soda/__init__.py
@@ -12,5 +12,46 @@ def compiles():
     check50.c.compile("soda.c", lcs50=True)
 
 @check50.check(compiles)
-def starts_with_cents_owed():
+def starts_with_owed():
+    """starts by printing how much is owed"""
     check50.run("./soda").stdout("50 cents owed")
+    
+@check50.check(compiles)
+def two_coins():
+    """accepts two 25 cents coins then prints 0 owed"""
+    (check50.run("./soda")
+        .stdout("50 cents owed")
+        .stdin("25")
+        .stdout("25 cents owed")
+        .stdin("25")
+        .stdout("0 cents change"))
+    
+@check50.check(compiles)
+def handles_wrong_coin():
+    """ignores invalid coins"""
+    (check50.run("./soda")
+        .stdout("50 cents owed")
+        .stdin("15")
+        .stdout("50 cents owed")
+        .stdin("3")
+        .stdout("50 cents owed")
+        .stdin("25")
+        .stdout("25 cents owed")
+        .stdin("30")
+        .stdout("25 cents owed")
+        .stdin("25")
+        .stdout("0 cents change"))
+    
+@check50.check(compiles)
+def gives_back_change():
+    """gives back change"""
+    (check50.run("./soda")
+        .stdout("50 cents owed")
+        .stdin("25")
+        .stdout("25 cents owed")
+        .stdin("10")
+        .stdout("15 cents owed")
+        .stdin("5")
+        .stdout("10 cents owed")
+        .stdin("25")
+        .stdout("15 cents change"))
