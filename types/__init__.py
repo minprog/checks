ec8010c3a282f5489a1aab21f4b65df08292476c
Jelle van Assema <jelle.van.assema@gmail.com>
2022-11-01 13:10:16 +0100

    Update __init__.py

--- types/__init__.py
+++ types/__init__.py
@@ -8,20 +8,20 @@ def exists():
 @check50.check(exists)
 def mario_passes_mypy():
     """mario passes type checks."""
-    (check50.run("mypy --strict mario.py")
+    (check50.run("mypy --strict --ignore-missing-imports mario.py")
         .stdout("Success: no issues found in 1 source file")
         .exit(0))
 
 @check50.check(exists)
 def readability_passes_mypy():
     """readability passes type checks."""
-    (check50.run("mypy --strict readability.py")
+    (check50.run("mypy --strict --ignore-missing-imports readability.py")
         .stdout("Success: no issues found in 1 source file")
         .exit(0))
 
 @check50.check(exists)
 def dna_passes_mypy():
     """schuifpuzzel passes type checks."""
-    (check50.run("mypy --strict schuifpuzzel.py")
+    (check50.run("mypy --strict --ignore-missing-imports schuifpuzzel.py")
         .stdout("Success: no issues found in 1 source file")
         .exit(0))
