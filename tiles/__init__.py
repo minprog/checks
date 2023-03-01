195738de7bd45a4b1005594acb7a872fc664f43b
Jelle van Assema <jelle.van.assema@gmail.com>
2023-03-01 14:13:45 +0100

    refactor tiles, check board per line instead of per tile

--- tiles/__init__.py
+++ tiles/__init__.py
@@ -20,50 +20,50 @@ def compiles():
 @check50.check(compiles)
 def init3():
     """initializes a 3x3 board correctly."""
-    board = ["8", "7", "6",
-             "5", "4", "3",
-             "2", "1", "[_0]"]
-
     check = check50.run("./tiles 3")
-    for tile in board:
-        check.stdout(tile)
-    check.stdout("\n")
+    
+    board = [
+        [8, 7, 6],
+        [5, 4, 3],
+        [2, 1, 0]
+    ]
+    check_board(check, board)
 
 
 @check50.check(compiles)
 def init4():
     """initializes a 4x4 board correctly."""
-
-    # check for board without swapped tiles
-    invalid_board = ["15", "14", "13", "12",
-                     "11", "10", "9", "8",
-                     "7", "6", "5", "4",
-                     "3", "2", "1", "[_0]"]
-
     check = check50.run("./tiles 4")
-
-    try:
-        for tile in invalid_board:
-            check.stdout(tile)
-        check.stdout("\n")
-    except:
-        # this test is supposed to raise an exception
-        pass
-    else:
-        # raise exception if the incorrect board is found
-        raise check50.Failure("board initialized incorrectly", help="did you remember to swap the last two tiles?")
-
-
+    
     # check for fully correct board
-    board = ["15", "14", "13", "12",
-             "11", "10", "9", "8",
-             "7", "6", "5", "4",
-             "3", "1", "2", "[_0]"]
+    correct_board = [
+        [15, 14, 13, 12],
+        [11, 10, 9, 8],
+        [7, 6, 5, 4],
+        [3, 1, 2, 0]
+    ]
+    
+    try:
+        check_board(check, correct_board)
+    except check50.Failure as f:
+        # check for board without swapped tiles
+        invalid_board = [
+            [15, 14, 13, 12],
+            [11, 10, 9, 8],
+            [7, 6, 5, 4],
+            [3, 2, 1, 0]
+        ]
 
-    check = check50.run("./tiles 4")
-    for tile in board:
-        check.stdout(tile)
-    check.stdout("\n")
+        check = check50.run("./tiles 4")
+
+        try:    
+            check_board(check, invalid_board)
+        except check50.Failure:
+            pass
+        else:
+            raise check50.Failure("board initialized incorrectly", help="did you remember to swap the last two tiles?")
+
+        raise f
 
 
 @check50.check(init3)
@@ -76,93 +76,102 @@ def invalid():
     for move in moves:
         check.stdin(move).stdout("Illegal move.")
 
-    board = ["8", "7", "6",
-             "5", "4", "3",
-             "2", "1", "[_0]"]
-    for tile in board:
-        check.stdout(tile)
-    check.stdout("\n")
+    board = [
+        [8, 7, 6],
+        [5, 4, 3],
+        [2, 1, 0]
+    ]
+
+    check_board(check, board)
 
 
 @check50.check(init3)
 def move_up2():
     """3x3 board: move blank up twice."""
     check = check50.run("./tiles 3").stdin("3")
-    board = ["8", "7", "6",
-             "5", "4", "[_0]",
-             "2", "1", "3"]
-    for tile in board:
-        check.stdout(tile)
-    check.stdout("\n")
+    
+    board = [
+        [8, 7, 6],
+        [5, 4, 0],
+        [2, 1, 3]
+    ]
+    check_board(check, board)
 
     check.stdin("6")
-    board = ["8", "7", "[_0]",
-             "5", "4", "6",
-             "2", "1", "3"]
-    for tile in board:
-        check.stdout(tile)
-    check.stdout("\n")
+    
+    board = [
+        [8, 7, 0],
+        [5, 4, 6],
+        [2, 1, 3]
+    ]
+    check_board(check, board)
 
 
 @check50.check(init3)
 def move_left2():
     """3x3 board: move blank left twice."""
     check = check50.run("./tiles 3").stdin("1")
-    board = ["8", "7", "6",
-             "5", "4", "3",
-             "2", "[_0]", "1"]
-    for tile in board:
-        check.stdout(tile)
-    check.stdout("\n")
+    
+    board = [
+        [8, 7, 6],
+        [5, 4, 3],
+        [2, 0, 1]
+    ]
+    check_board(check, board)
 
     check.stdin("2")
-    board = ["8", "7", "6",
-             "5", "4", "3",
-             "[_0]", "2", "1"]
-    for tile in board:
-        check.stdout(tile)
-    check.stdout("\n")
+    
+    board = [
+        [8, 7, 6],
+        [5, 4, 3],
+        [0, 2, 1]
+    ]
+    check_board(check, board)
 
 
 @check50.check(init3)
 def move_left_right():
     """3x3 board: move blank left then right."""
     check = check50.run("./tiles 3").stdin("1")
-    board = ["8", "7", "6",
-             "5", "4", "3",
-             "2", "[_0]", "1"]
-    for tile in board:
-        check.stdout(tile)
-    check.stdout("\n")
+    
+    board = [
+        [8, 7, 6],
+        [5, 4, 3],
+        [2, 0, 1]
+    ]
+    check_board(check, board)
 
     check.stdin("1")
-    board = ["8", "7", "6",
-             "5", "4", "3",
-             "2", "1", "[_0]"]
-    for tile in board:
-        check.stdout(tile)
-    check.stdout("\n")
+    
+    board = [
+        [8, 7, 6],
+        [5, 4, 3],
+        [2, 1, 0]
+    ]
+    check_board(check, board)
 
 
 @check50.check(init3)
 def move_up_down():
     """3x3 board: move blank up then down."""
     check = check50.run("./tiles 3").stdin("3")
-    board = ["8", "7", "6",
-             "5", "4", "[_0]",
-             "2", "1", "3"]
-    for tile in board:
-        check.stdout(tile)
-    check.stdout("\n")
-
+    
+    board = [
+        [8, 7, 6],
+        [5, 4, 0],
+        [2, 1, 3]
+    ]
+    check_board(check, board)
+    
     check.stdin("3")
-    board = ["8", "7", "6",
-             "5", "4", "3",
-             "2", "1", "[_0]"]
-    for tile in board:
-        check.stdout(tile)
-    check.stdout("\n")
-
+    
+    board = [
+        [8, 7, 6],
+        [5, 4, 3],
+        [2, 1, 0]
+    ]
+    check_board(check, board)
+    
 
 @check50.check(init3)
 def invalid_center():
@@ -171,33 +180,31 @@ def invalid_center():
     check = check50.run("./tiles 3").stdin("1")
 
     # check resulting board
-    board = ["8", "7", "6",
-             "5", "4", "3",
-             "2", "[_0]", "1"]
-    for tile in board:
-        check.stdout(tile)
-    check.stdout("\n")
+    board = [
+        [8, 7, 6],
+        [5, 4, 3],
+        [2, 0, 1]
+    ]
+    check_board(check, board)
 
     # move 4
     check.stdin("4")
 
     # check resulting board
-    board = ["8", "7", "6",
-             "5", "[_0]", "3",
-             "2", "4", "1"]
-    for tile in board:
-        check.stdout(tile)
-    check.stdout("\n")
+    board = [
+        [8, 7, 6],
+        [5, 0, 3],
+        [2, 4, 1]
+    ]
+    check_board(check, board)
 
     # try moving all corner tiles
     for move in ["1", "2", "6", "8"]:
         check.stdin(move).stdout("Illegal move.")
 
     # check that board state didn't change
-    for tile in board:
-        check.stdout(tile)
-    check.stdout("\n")
-
+    check_board(check, board)
+    
 
 @check50.check(init3)
 def solve3():
@@ -210,18 +217,18 @@ def solve3():
              "4","5","6","7","8","6","5","4",
              "7","8"]
 
-    board = ["1", "2", "3",
-             "4", "5", "6",
-             "7", "8", "[_0]"]
-
     check = check50.run("./tiles 3")
 
     for step in steps:
         check.stdout("Tile to move:")
         check.stdin(step, prompt=False)
 
-    for tile in board:
-        check.stdout(tile)
+    board = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 0]
+    ]
+    check_board(check, board)
     check.stdout("ftw!")
 
 
@@ -247,17 +254,24 @@ def solve4():
              "11","14","12","11","15","10",
              "14","15","11","12"]
 
-    board = ["1", "2", "3", "4",
-             "5", "6", "7", "8",
-             "9", "10", "11", "12",
-             "13", "14", "15", "[_0]"]
-
     check = check50.run("./tiles 4")
 
     for step in steps:
         check.stdout("Tile to move:")
         check.stdin(step, prompt=False)
 
-    for tile in board:
-        check.stdout(tile)
+    board = [
+        [1, 2, 3, 4],
+        [5, 6, 7, 8],
+        [9, 10, 11, 12],
+        [13, 14, 15, 0]
+    ]
+    check_board(check, board)
     check.stdout("ftw!")
+
+def check_board(check, board):
+    """Helper function to check that each row of the board is correct"""
+    for row in board:
+        row_regex = "[^\n]+".join(str(n) if n != 0 else "[_0]" for n in row) + "[^\n]*\n"
+        row_str = "    ".join(str(n).rjust(2) for n in row)
+        check.stdout(row_regex, str_output=row_str)
