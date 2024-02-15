59d7c17158e4f69b48b291331d46980948406d1e
jelleas <jelle.van.assema@gmail.com>
2024-02-15 17:25:03 +0100

    unix/wc checks

--- unix/wc/__init__.py
+++ unix/wc/__init__.py
@@ -21,16 +21,19 @@ def compiles():
 @check50.check(compiles)
 def test_wc_wc():
     """./wc wc.c prints the exact same as: wc wc.c"""
-    out_real = check50.run("./wc wc.c").stdout()
-    out_expected = check50.run("wc wc.c").stdout()
+    # wc uses different whitespaces on Mac/Linux
+    out_real = " ".join(check50.run("./wc wc.c").stdout().split())
+    out_expected = " ".join(check50.run("wc wc.c").stdout().split())
     assert_same(out_expected, out_real)
 
 @check50.check(compiles)
 def test_wc_foo():
-    """echo "hello\\nworld\\n" > foo.c && ./wc foo.c prints:       3       2      13 foo.c"""
+    """echo "hello\\nworld\\n" > foo.c && ./wc foo.c prints the exact same as: wc foo.c"""
     check50.run('echo "hello\nworld\n" > foo.c').exit(0)
-    out_real = check50.run("./wc foo.c").stdout()
-    out_expected = check50.run("wc foo.c").stdout()
+
+    # wc uses different whitespaces on Mac/Linux
+    out_real = " ".join(check50.run("./wc foo.c").stdout().split())
+    out_expected = " ".join(check50.run("wc foo.c").stdout().split())
     assert_same(out_expected, out_real)
 
 def assert_same(expected: str, real: str):
