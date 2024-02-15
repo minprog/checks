f1a0d316e5a54dd0e38e682ea23cd5b702e000d8
jelleas <jelle.van.assema@gmail.com>
2024-02-15 13:55:21 +0100

    unix/cat checks

---
+++ unix/cat/__init__.py
@@ -0,0 +1,38 @@
+import check50
+import check50.c
+import check50.internal
+
+helpers = check50.internal.import_file(
+    "helpers",
+    check50.internal.check_dir / "../../helpers/helpers.py"
+)
+helpers.set_stdout_limit(100)
+
+@check50.check()
+def exists():
+    """cat.c exists"""
+    check50.exists("cat.c")
+
+@check50.check(exists)
+def compiles():
+    """cat.c compiles"""
+    check50.c.compile("cat.c", lcs50=True)
+
+@check50.check(compiles)
+def test_cat_cat():
+    """./cat cat.c prints contents of cat.c"""
+    out_real = check50.run("./cat cat.c").stdout()
+    out_expected = check50.run("cat cat.c").stdout()
+
+    if out_expected != out_real:
+        raise check50.Mismatch(out_expected, out_real)
+
+@check50.check(compiles)
+def test_cat_foo():
+    """echo "hello\\n\world\\n" > foo.c && ./cat foo.c prints: hello\\n\world\\n"""
+    check50.run('echo "hello\n\world\n" > foo.c').exit(0)
+    out_real = check50.run("./cat foo.c").stdout()
+    out_expected = check50.run("cat foo.c").stdout()
+
+    if out_expected != out_real:
+        raise check50.Mismatch(out_expected, out_real)
