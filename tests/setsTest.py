11a18efb01028f62e83b4ec6696f2abe93347736
jelleas <jelle.van.assema@gmail.com>
2024-06-27 14:29:08 +0200

    sets tests

--- tests/setsTest.py
+++ tests/setsTest.py
@@ -34,7 +34,7 @@ def testUnion():
     for call in calls:
         if call.endswith((".union")):
             raise AssertionError(
-                "don't use python's built-in union method in this assignment"
+                "don't use python's built-in union method for this assignment"
             )
     
     # Get union's function definition
@@ -52,15 +52,14 @@ def testUnion():
             if node.value != None:
                 self.visit(node.value)
             
-
         def visit_BinOp(self, node: ast.BinOp):
             # Assert ast.BitOr is not used outside of ast.AnnAssign
             if type(node.op) == ast.BitOr:
                 raise AssertionError(
-                    f"don't use python's built-in | operator in this assignment, found on line number: {node.lineno}"
+                    f"don't use python's built-in | operator for this assignment, found on line number: {node.lineno}"
                 )
 
-    # Visit each statement in Unions function definition
+    # Visit each statement in union's function definition
     for stmt in unionDef.body:
         UnionVisitor().visit(stmt)
 
@@ -79,6 +78,81 @@ def testUnion():
         .returns({"hello", 4, 5})
     )()
 
+@passed(testTests, hide=False)
+def testIntersection():
+    """intersection werkt correct"""
+    calls = static.getFunctionCalls()
+    for call in calls:
+        if call.endswith((".intersection")):
+            raise AssertionError(
+                "don't use python's built-in intersection method for this assignment"
+            )
+
+    assert ast.BitAnd not in static.AbstractSyntaxTree(), \
+        "don't use python's built-in & operator for this assignment"
+
+    (declarative.function("intersection")
+        .params("set_a", "set_b")
+        .returnType(set)
+        .call({1, 2, 3}, {2, 3, 4})
+        .returns({2, 3})
+        .call({1, 2, 3}, {1, 2, 3})
+        .returns({1, 2, 3})
+        .call({2}, set())
+        .returns(set())
+        .call(set(), set())
+        .returns(set())
+        .call({1, "foo"}, {1})
+        .returns({1})
+    )()
+
+@passed(testTests, hide=False)
+def testDifference():
+    """difference werkt correct"""
+    calls = static.getFunctionCalls()
+    for call in calls:
+        if call.endswith((".difference")):
+            raise AssertionError(
+                "don't use python's built-in difference method for this assignment"
+            )
+    
+    # Get difference's function definition
+    functionDefinitions = static.getAstNodes(ast.FunctionDef)
+    functionDefinitions = [f for f in functionDefinitions if f.name == "difference"]
+    if not functionDefinitions:
+        raise AssertionError(
+            f"difference is not defined in {file.name}"
+        )
+    differenceDef = functionDefinitions[0]
+
+    class DifferenceVisitor(ast.NodeVisitor):
+        def visit_BinOp(self, node: ast.BinOp):
+            # Assert ast.Sub is not used
+            if type(node.op) == ast.Sub:
+                raise AssertionError(
+                    f"don't use python's built-in - operator for this assignment, found on line number: {node.lineno}"
+                )
+
+    # Visit each statement in difference's function definition
+    for stmt in differenceDef.body:
+        DifferenceVisitor().visit(stmt)
+    
+    (declarative.function("difference")
+        .params("set_a", "set_b")
+        .returnType(set)
+        .call({1, 2, 3}, {2, 3, 4})
+        .returns({1})
+        .call({1, 2, 3}, {1, 2, 3})
+        .returns(set())
+        .call({2}, set())
+        .returns({2})
+        .call(set(), {2})
+        .returns(set())
+        .call(set(), set())
+        .returns(set())
+        .call({1, "foo"}, {1})
+        .returns({"foo"})
+    )()
 
 @passed(testTests, hide=False)
 def testSymmetricDifference():
@@ -87,12 +161,11 @@ def testSymmetricDifference():
     for call in calls:
         if call.endswith((".symmetric_difference")):
             raise AssertionError(
-                "don't use python's built-in symmetric_difference method in this assignment"
+                "don't use python's built-in symmetric_difference method for this assignment"
             )
     
-    assert \
-        ast.BitXor not in static.AbstractSyntaxTree(), \
-        "don't use python's built-in ^ operator in this assignment"
+    assert ast.BitXor not in static.AbstractSyntaxTree(), \
+        "don't use python's built-in ^ operator for this assignment"
     
     (declarative.function("symmetric_difference")
         .params("set_a", "set_b")
