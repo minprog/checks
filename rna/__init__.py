9e498bc22257296a1c68da68d2b6bbbc900235cb
jelleas <jelle.van.assema@gmail.com>
2025-02-12 15:41:09 +0100

    rm argv from rna

--- rna/__init__.py
+++ rna/__init__.py
@@ -14,25 +14,28 @@ def compiles():
 @check50.check(compiles)
 def testATGC():
     """transcribes ATGC as UACG"""
-    (check50.run("./rna ATGC")
+    (check50.run("./rna")
+        .stdin("ATGC")
         .stdout("UACG", str_output="UACG"))
 
 @check50.check(testATGC)
 def testAAGGTTCCAA():
     """transcribes AAGGTTCCAA as UUCCAAGGUU"""
-    (check50.run("./rna AAGGTTCCAA")
+    (check50.run("./rna")
+        .stdin("AAGGTTCCAA")
         .stdout("UUCCAAGGUU", str_output="UUCCAAGGUU"))
 
 @check50.check(testAAGGTTCCAA)
 def testCGaT():
     """transcribes CGaT as GCUA"""
-    (check50.run("./rna CGaT")
+    (check50.run("./rna")
+        .stdin("CGaT")
         .stdout("GCUA", str_output="GCUA"))
 
 @check50.check(testCGaT)
 def testAAF():
     """handles invalid input (AAF)"""
-    out = check50.run("./rna AAF").stdout()
+    out = check50.run("./rna").stdin("AAF").stdout()
     out = out.strip()
 
     if out != "Invalid DNA" and out != "invalid DNA":
@@ -41,19 +44,8 @@ def testAAF():
 @check50.check(testCGaT)
 def testZATG():
     """handles invalid input (ZATG)"""
-    out = check50.run("./rna ZATG").stdout()
+    out = check50.run("./rna").stdin("ZATG").stdout()
     out = out.strip()
 
     if out != "Invalid DNA" and out != "invalid DNA":
         raise check50.Failure(f"expected Invalid DNA, but found {out}")
-
-
-@check50.check(testAAF)
-def handles_no_argv():
-    """handles lack of argv[1]"""
-    check50.run("./rna").stdout("[U|u]sage: ./rna ATGC", str_output="Usage: ./rna ATGC").exit(1)
-
-@check50.check(testAAF)
-def handles_too_many_argv():
-    """handles having too many command-line arguments"""
-    check50.run("./rna ATGC ATGC").stdout("[U|u]sage: ./rna ATGC", str_output="Usage: ./rna ATGC").exit(1)
