78ea1a9f93dadfa2d794a0351971850330f5d2ab
Martijn Stegeman <martijn@stgm.nl>
2025-02-17 16:35:23 +0100

    calendar 2025

--- calendar/__init__.py
+++ calendar/__init__.py
@@ -7,26 +7,26 @@ import glob
 @check50.check()
 def exists():
     """calendar.c exists"""
-    check50.exists("calendar.c")
+    check50.exists('calendar.c')
 
 @check50.check(exists)
 def compiles():
     """calendar.c compiles"""
-    check50.c.compile("calendar.c", lcs50=True)
+    check50.c.compile('calendar.c', lcs50=True)
 
 
 @check50.check(compiles)
 def cal_1_1800():
     """displays the correct calendar for Jan 1800"""
-    check = check50.run("./calendar 1800 1")
-    check_calendar_output(check, "Jan 1800", 3, 31)
+    check = check50.run('./calendar').stdin('1800').stdin('1')
+    check_calendar_output(check, 'Jan 1800', 3, 31)
 
 
 @check50.check(cal_1_1800)
 def cal_11_2021():
     """displays the correct calendar for Nov 2021"""
-    check = check50.run("./calendar 2021 11")
-    check_calendar_output(check, "Nov 2021", 1, 30)
+    check = check50.run('./calendar').stdin('2021').stdin('11')
+    check_calendar_output(check, 'Nov 2021', 1, 30)
 
 
 @check50.check(cal_11_2021)
@@ -34,27 +34,27 @@ def cal_leap_years():
     """displays the correct calendar for Feb/Mar in leap years"""
 
     # not a leap year
-    check = check50.run("./calendar 1900 2")
-    check_calendar_output(check, "Feb 1900", 4, 28)
+    check = check50.run('./calendar').stdin('1900').stdin('2')
+    check_calendar_output(check, 'Feb 1900', 4, 28)
 
     # march in a leap year
-    check = check50.run("./calendar 2016 3")
-    check_calendar_output(check, "Mar 2016", 2, 31)
+    check = check50.run('./calendar').stdin('2016').stdin('3')
+    check_calendar_output(check, 'Mar 2016', 2, 31)
 
     # special leap year
-    check = check50.run("./calendar 2000 2")
-    check_calendar_output(check, "Feb 2000", 2, 29)
+    check = check50.run('./calendar').stdin('2000').stdin('2')
+    check_calendar_output(check, 'Feb 2000', 2, 29)
 
     # march in that special leap year
-    check = check50.run("./calendar 2000 3")
-    check_calendar_output(check, "Mar 2000", 3, 31)
+    check = check50.run('./calendar').stdin('2000').stdin('3')
+    check_calendar_output(check, 'Mar 2000', 3, 31)
 
 
-@check50.check(compiles)
-def rejects_wrong_command_line():
-    """rejects any wrong number of command-line arguments"""
-    check50.run("./calendar").stdout("Usage: ./calendar year month").exit(1)
-    check50.run("./calendar 2021 11 -l").stdout("Usage: ./calendar year month").exit(1)
+# @check50.check(compiles)
+# def rejects_wrong_command_line():
+#     """rejects any wrong number of command-line arguments"""
+#     check50.run('./calendar').stdout('Usage: ./calendar year month').exit(1)
+#     check50.run('./calendar 2021 11 -l').stdout('Usage: ./calendar year month').exit(1)
 
 
 def check_calendar_output(check, month_display, n_padding, n_days):
@@ -62,19 +62,19 @@ def check_calendar_output(check, month_display, n_padding, n_days):
     check.stdout(month_display)
 
     # weekdays until first day of month
-    check.stdout("Sun Mon Tue Wed Thu Fri Sat")
+    check.stdout('Sun Mon Tue Wed Thu Fri Sat')
 
     # padding before first day
-    check.stdout("\n" + "    " * n_padding + format(1, "3d"), "<padding> 1")
+    check.stdout('\n' + '    ' * n_padding + format(1, '3d'), '<padding> 1')
 
     # all days but last
     for daynum in range(2, n_days):
-        check.stdout(format(daynum, "3d"), format(daynum, "d"))
+        check.stdout(format(daynum, '3d'), format(daynum, 'd'))
 
     # final day + newline
-    check.stdout(f"{n_days}\s*\n", f"{n_days}")
+    check.stdout(f'{n_days}\\s*\n', f'{n_days}')
 
     # make sure nothing follows
     remainder = str(check.stdout())
     if len(remainder) > 0:
-        raise check50.Failure("expected end of output after the last day of the month")
+        raise check50.Failure('expected end of output after the last day of the month')
