2f45bd1318f4900fcce1f0be36b23a4ba9a9d85a
jelleas <jelle.van.assema@gmail.com>
2025-02-17 14:43:54 +0100

    decryptor fix

--- decryptor/__init__.py
+++ decryptor/__init__.py
@@ -3,6 +3,12 @@ import check50.c
 import check50.internal
 import contextlib
 
+helpers = check50.internal.import_file(
+    "helpers",
+    check50.internal.check_dir / "../helpers/helpers.py"
+)
+helpers.set_stdout_limit(10000)
+
 @check50.check()
 def exists():
     """decryptor.c exists"""
@@ -22,19 +28,23 @@ def test_base_case():
         .exit(0))
 
 @check50.check(test_base_case)
-def test_output():
-    """handles automatic decryption of messsages (we think)"""
+def test_output1():
+    """handles automatic decryption of messsages (Lbsdscr dbyyzc oxdobon Mehrkfox 1400 yx 6 Wki)"""
     with logged_check_factory("./decryptor") as decryptor_command:
         (decryptor_command("")
             .stdin("Lbsdscr dbyyzc oxdobon Mehrkfox 1400 yx 6 Wki")
             .stdout("British troops entered Cuxhaven 1400 on 6 May", regex=False)
             .exit(0))
-        
+
+@check50.check(test_base_case)
+def test_output2():
+    """handles automatic decryption of messsages (Rhyjyix jheefi udjuhut Sknxqlud qj 1400 ed 6 Cqo - vhec dem ed qbb hqtye jhqvvys mybb suqiu - myixydw oek qbb jxu ruij. Bj Akdaub 20)"""
+    with logged_check_factory("./decryptor") as decryptor_command:
         (decryptor_command("")
             .stdin("Rhyjyix jheefi udjuhut Sknxqlud qj 1400 ed 6 Cqo - vhec dem ed qbb hqtye jhqvvys mybb suqiu - myixydw oek qbb jxu ruij. Bj Akdaub 20")
             .stdout("British troops entered Cuxhaven at 1400 on 6 May - from now on all radio traffic will cease - wishing you all the best. Lt Kunkel 20", regex=False)
             .exit(0))
-
+        
 # helpers --------------------------------------------------------------------
 
 class Stream:
