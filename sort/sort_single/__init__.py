3a373eb0f13b995b8d5b45f80fea5c1d097faf78
jelleas <jelle.van.assema@gmail.com>
2025-02-24 12:03:03 +0100

    sort_single

---
+++ sort/sort_single/__init__.py
@@ -0,0 +1,54 @@
+import check50
+import check50.c
+
+@check50.check()
+def exists():
+    """sort.c exists."""
+    check50.exists("sort.c")
+
+
+@check50.check(exists)
+def compiles():
+    """sort.c compiles."""
+    check50.c.compile("sort.c", lcs50=True)
+
+
+@check50.check(compiles)
+def sort_5():
+    """sorts 5 numbers"""
+    test_sorted(5)
+
+
+@check50.check(compiles)
+def sort_1:
+    """sorts 1 number"""
+    test_sorted(1)
+
+
+@check50.check(compiles)
+def sort_6():
+    """sorts 6 numbers"""
+    test_sorted(6)
+
+
+def test_sorted(n_items: int):
+    out = check50.run(f"./sort {n_items}").stdout()
+
+    prev_number = float("-inf")
+
+    for i, line in enumerate(out.split("\n")):
+        line = line.strip()
+        if line:
+            try:
+                number = int(line)
+            except ValueError:
+                raise check50.Failure(
+                    "expected a number line on each line of output, but found {line}"
+                )
+            
+            if prev_number > number:
+                raise check50.Failure(
+                    "expected each following number to be bigger, but found {prev_number} and {number}"
+                )
+            
+            prev_number = number
