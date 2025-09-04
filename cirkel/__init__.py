1ed22a5c58198d5e8a5678375784577f4d4303c8
jelleas <jelle.van.assema@gmail.com>
2025-09-04 11:57:19 +0200

    fix warning

--- cirkel/__init__.py
+++ cirkel/__init__.py
@@ -1,6 +1,12 @@
 import check50
 import check50.c
 
+helpers = check50.internal.import_file(
+    "helpers",
+    check50.internal.check_dir / "../helpers/helpers.py"
+)
+helpers.set_stdout_limit(10000)
+
 @check50.check()
 def exists():
     """cirkel.c exists"""
@@ -15,46 +21,46 @@ def compiles():
 def test_cirkel5():
     """cirkel met hoogte 5 is correct"""
     answer = (
-        "   xxxxx\s*\n"   
-        "  x     x\s*\n"  
-        " x       x\s*\n" 
-        "x         x\s*\n"
-        "x         x\s*\n"
-        "x         x\s*\n"
-        "x         x\s*\n"
-        "x         x\s*\n"
-        " x       x\s*\n" 
-        "  x     x\s*\n"  
-        "   xxxxx\s*\n" 
+        r"   xxxxx\s*\n"   
+        r"  x     x\s*\n"  
+        r" x       x\s*\n" 
+        r"x         x\s*\n"
+        r"x         x\s*\n"
+        r"x         x\s*\n"
+        r"x         x\s*\n"
+        r"x         x\s*\n"
+        r" x       x\s*\n" 
+        r"  x     x\s*\n"  
+        r"   xxxxx\s*\n" 
     )
-    readable_answer = answer.replace("\s*", "")
+    readable_answer = answer.replace(r"\s*", "")
     check50.run("./cirkel").stdin("5").stdout(answer, str_output=readable_answer).exit(0)
 
 @check50.check(compiles)
 def test_cirkel10():
     """cirkel met hoogte 10 is correct"""
     answer = (
-        "       xxxxxxx\s*\n"
-        "     xx       xx\s*\n"
-        "    x           x\s*\n"
-        "   x             x\s*\n"   
-        "  x               x\s*\n"  
-        " x                 x\s*\n" 
-        " x                 x\s*\n" 
-        "x                   x\s*\n"
-        "x                   x\s*\n"
-        "x                   x\s*\n"
-        "x                   x\s*\n"
-        "x                   x\s*\n"
-        "x                   x\s*\n"
-        "x                   x\s*\n"
-        " x                 x\s*\n" 
-        " x                 x\s*\n" 
-        "  x               x\s*\n"  
-        "   x             x\s*\n"   
-        "    x           x\s*\n"    
-        "     xx       xx\s*\n"     
-        "       xxxxxxx\s*\n"
+        r"       xxxxxxx\s*\n"
+        r"     xx       xx\s*\n"
+        r"    x           x\s*\n"
+        r"   x             x\s*\n"   
+        r"  x               x\s*\n"  
+        r" x                 x\s*\n" 
+        r" x                 x\s*\n" 
+        r"x                   x\s*\n"
+        r"x                   x\s*\n"
+        r"x                   x\s*\n"
+        r"x                   x\s*\n"
+        r"x                   x\s*\n"
+        r"x                   x\s*\n"
+        r"x                   x\s*\n"
+        r" x                 x\s*\n" 
+        r" x                 x\s*\n" 
+        r"  x               x\s*\n"  
+        r"   x             x\s*\n"   
+        r"    x           x\s*\n"    
+        r"     xx       xx\s*\n"     
+        r"       xxxxxxx\s*\n"
     )
     readable_answer = answer.replace(r"\s*", "")
     check50.run("./cirkel").stdin("10").stdout(answer, str_output=readable_answer).exit(0)
@@ -63,17 +69,17 @@ def test_cirkel10():
 def test_cirkel_incorrect_input():
     """vraagt opnieuw om input bij een incorrecte hoogte van 3"""
     answer = (
-        "   xxxxx\s*\n"   
-        "  x     x\s*\n"  
-        " x       x\s*\n" 
-        "x         x\s*\n"
-        "x         x\s*\n"
-        "x         x\s*\n"
-        "x         x\s*\n"
-        "x         x\s*\n"
-        " x       x\s*\n" 
-        "  x     x\s*\n"  
-        "   xxxxx\s*\n" 
+        r"   xxxxx\s*\n"   
+        r"  x     x\s*\n"  
+        r" x       x\s*\n" 
+        r"x         x\s*\n"
+        r"x         x\s*\n"
+        r"x         x\s*\n"
+        r"x         x\s*\n"
+        r"x         x\s*\n"
+        r" x       x\s*\n" 
+        r"  x     x\s*\n"  
+        r"   xxxxx\s*\n" 
     )
-    readable_answer = answer.replace("\s*", "")
+    readable_answer = answer.replace(r"\s*", "")
     check50.run("./cirkel").stdin("3").stdin("5").stdout(answer, str_output=readable_answer).exit(0)
