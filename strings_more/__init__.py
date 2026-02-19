00d5c49742fec19f6702004fcbc93109dff39636
jelleas <jelle.van.assema@gmail.com>
2026-02-19 12:34:57 +0100

    taboo in place check

--- strings_more/__init__.py
+++ strings_more/__init__.py
@@ -67,6 +67,31 @@ def test_is_palindrome_false():
         check50.run("./strings_more").stdout("false", regex=False).exit(0)
 
 @check50.check(has_functions)
+def test_taboo_string_modification():
+    """taboo does >>not< modify the string in place"""
+    main = (
+        'int main(void) {\n'
+        '    char original[] = "The cat is on the roof.";\n'
+        '    char copy[] = "The cat is on the roof.";\n'
+        '    taboo(copy);\n'
+        '    for (int i = 0, n = strlen(original); i < n; i++) {\n'
+        '        if (original[i] != copy[i]) {\n'
+        '            printf("String modified in place!\\n%s became %s\\n", original, copy);\n'
+        '            return 0;\n'
+        '        }\n'
+        '    }\n'
+        '}'
+    )
+
+    with helpers.replace_main("strings_more.c", main):
+        check50.c.compile("strings_more.c", lcs50=True)
+        output = check50.run("./strings_more").stdout()
+
+        marker = "String modified in place!"
+        if marker in output:
+            raise check50.Failure(output[output.find(marker):].strip())
+
+@check50.check(test_taboo_string_modification)
 def test_taboo():
     """taboo("The Cheshire Cat is the cat of the Duchess.") prints The Cheshire Dog is the dog of the Duchess."""
     main = (
@@ -79,7 +104,7 @@ def test_taboo():
         check50.c.compile("strings_more.c", lcs50=True)
         check50.run("./strings_more").stdout("The Cheshire Dog is the dog of the Duchess.", regex=False).exit(0)
 
-@check50.check(has_functions)
+@check50.check(test_taboo_string_modification)
 def test_taboo_plural():
     """taboo("cat cat cat or the cats of the Duchess.") prints dog dog dog or the cats of the Duchess."""
     main = (
