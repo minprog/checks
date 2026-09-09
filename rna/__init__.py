3aa85293416e27610c612d5b162e46a3fb7cce66
jelleas <jelle.van.assema@gmail.com>
2026-09-09 15:05:26 +0200

    command-line RNA

--- rna/__init__.py
+++ rna/__init__.py
@@ -12,40 +12,71 @@ def compiles():
     check50.c.compile("rna.c", "-lcs50")
 
 @check50.check(compiles)
+def testUsage():
+    """handles a missing command-line argument"""
+    process = check50.run("./rna")
+    out = process.stdout().strip()
+
+    if out != "Usage: ./rna <DNA>":
+        raise check50.Failure(f"expected Usage: ./rna <DNA>, but found {out}")
+
+    check50.log("checking that program exited with status 1...")
+    if process.exitcode != 1:
+        raise check50.Failure(f"expected exit code 1, not {process.exitcode}")
+
+@check50.check(compiles)
+def testUsageTooManyArgs():
+    """handles too many command-line arguments"""
+    process = check50.run("./rna ATGC ATGC")
+    out = process.stdout().strip()
+
+    if out != "Usage: ./rna <DNA>":
+        raise check50.Failure(f"expected Usage: ./rna <DNA>, but found {out}")
+
+    check50.log("checking that program exited with status 1...")
+    if process.exitcode != 1:
+        raise check50.Failure(f"expected exit code 1, not {process.exitcode}")
+
+@check50.check(compiles)
 def testATGC():
     """transcribes ATGC as UACG"""
-    (check50.run("./rna")
-        .stdin("ATGC")
+    (check50.run("./rna ATGC")
         .stdout("UACG", str_output="UACG"))
 
 @check50.check(testATGC)
 def testAAGGTTCCAA():
     """transcribes AAGGTTCCAA as UUCCAAGGUU"""
-    (check50.run("./rna")
-        .stdin("AAGGTTCCAA")
+    (check50.run("./rna AAGGTTCCAA")
         .stdout("UUCCAAGGUU", str_output="UUCCAAGGUU"))
 
 @check50.check(testAAGGTTCCAA)
 def testCGaT():
     """transcribes CGaT as GCUA"""
-    (check50.run("./rna")
-        .stdin("CGaT")
+    (check50.run("./rna CGaT")
         .stdout("GCUA", str_output="GCUA"))
 
 @check50.check(testCGaT)
 def testAAF():
     """handles invalid input (AAF)"""
-    out = check50.run("./rna").stdin("AAF").stdout()
-    out = out.strip()
+    process = check50.run("./rna AAF")
+    out = process.stdout().strip()
 
     if out != "Invalid DNA" and out != "invalid DNA":
         raise check50.Failure(f"expected Invalid DNA, but found {out}")
+
+    check50.log("checking that program exited with status 1...")
+    if process.exitcode != 1:
+        raise check50.Failure(f"expected exit code 1, not {process.exitcode}")
 
 @check50.check(testCGaT)
 def testZATG():
     """handles invalid input (ZATG)"""
-    out = check50.run("./rna").stdin("ZATG").stdout()
-    out = out.strip()
+    process = check50.run("./rna ZATG")
+    out = process.stdout().strip()
 
     if out != "Invalid DNA" and out != "invalid DNA":
         raise check50.Failure(f"expected Invalid DNA, but found {out}")
+
+    check50.log("checking that program exited with status 1...")
+    if process.exitcode != 1:
+        raise check50.Failure(f"expected exit code 1, not {process.exitcode}")
